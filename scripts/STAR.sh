fdc5614e98b82369efa93f1bce2d7492e05446b6
abportillo02 <abportillo@coh.org>
2025-10-03 15:32:19 -0700

    Rna pipeline

---
+++ scripts/STAR.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+sample_name=$1
+datapath=$2
+outdir=$3
+
+mkdir -p "$outdir"
+source /path/to/config.sh
+
+
+#### fastqc for fastq files
+mkdir -p ${outdir}/fastqc_out
+module load FastQC/0.11.8
+fastqc -t 8 -o ${outdir}/fastqc_out \
+${datapath_aging}/${sample_name}_1.fastq.gz ${datapath_aging}/${sample_name}_2.fastq.gz
+
+ln -s ${datapath_aging}/${sample_name}_1.fastq.gz ${outdir}/${sample_name}_R1.fastq.gz
+ln -s ${datapath_aging}/${sample_name}_2.fastq.gz ${outdir}/${sample_name}_R2.fastq.gz
+module unload FastQC/0.11.8
+### Align reads with STAR:
+${STAR} --genomeDir /home/abportillo/github_repo/RNA_seq_Bcell/scripts/raw_fastq_bcell/rnaPreprocess/hg38_p14/STAR_hg38_p14_geneCodeGTF_filter \
+--readFilesIn ${datapath_aging}/${sample_name}_1.fastq.gz ${datapath_aging}/${sample_name}_2.fastq.gz \
+--readFilesCommand zcat \
+--runThreadN 8 \
+--twopassMode Basic \
+--outFilterMultimapNmax 20 \
+--alignSJoverhangMin 8 \
+--alignSJDBoverhangMin 1 \
+--outFilterMismatchNmax 999 \
+--outFilterMismatchNoverLmax 0.1 \
+--alignIntronMin 20 \
+--alignIntronMax 1000000 \
+--alignMatesGapMax 1000000 \
+--outFilterType BySJout \
+--outFilterScoreMinOverLread 0.33 \
+--outFilterMatchNminOverLread 0.33 \
+--limitSjdbInsertNsj 1200000 \
+--outFileNamePrefix ${outdir}/${sample_name}_ \
+--outSAMstrandField intronMotif \
+--outFilterIntronMotifs None \
+--alignSoftClipAtReferenceEnds Yes \
+--quantMode TranscriptomeSAM GeneCounts \
+--outSAMtype BAM Unsorted \
+--outSAMunmapped Within \
+--genomeLoad NoSharedMemory \
+--chimSegmentMin 15 \
+--chimJunctionOverhangMin 15 \
+--chimOutType Junctions SeparateSAMold WithinBAM SoftClip \
+--chimOutJunctionFormat 1 \
+--chimMainSegmentMultNmax 1 \
+--outSAMattributes NH HI AS nM NM ch
+
+#--outSAMattrRGline ID:sample1 SM:sample1 PL:ILLUMINA LB:lib1 PU:unit1; not sure lib info. so leave it out
+#--outFilterMismatchNmax 10, not use in R1 and R2 because:
+# other para. covered this filtering, the default value is unlimited
+#--outFilterMultimapNmax 100, for repeat, not use in this round
+#--winAnchorMultimapNmax 100, for repeat, not use in this round
+# --outSAMmultNmax 25, for allo, not use in this round
+
---
+++ scripts/configure.sh
@@ -0,0 +1,10 @@
+  # config.sh
+java=/home/abportillo/.conda/envs/mamba_abner_BC/bin/java
+bamCoverage=/home/abportillo/.conda/envs/mamba_abner_BC/bin/bamCoverage
+samtools=/home/abportillo/.conda/envs/mamba_abner_BC/bin/samtools
+STAR=/home/abportillo/.conda/envs/mamba_abner_BC/bin/STAR
+picard=/home/abportillo/.conda/envs/mamba_abner_BC/bin/picard
+wigToBigWig=/home/abportillo/.conda/envs/mamba_abner_BC/bin/wigToBigWig
+
+source /home/abportillo/.bashrc
+conda activate /home/abportillo/.conda/envs/mamba_abner_BC
