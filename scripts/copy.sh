2b2db9e1776e10b7c3ce234dc12ee90b009bc385
abportillo02 <abportillo@coh.org>
2025-10-03 14:19:06 -0700

    copy

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Source and destination directories
 input="/net/nfs-irwrsrchnas01/labs/dschones/Seq/LaBarge-data/data/LEP"
 output="/home/abportillo/github_repo/Aging/bz2"
 
-# Create destination directory if it doesn't exist
+
 mkdir -p "$output"
 
-# Copy all .bz2 files
 mkdir -p "$bz2"
 find "$input" -type f -name "*.bz2" -exec cp {} "$output" \;
