5809861648cb6ec2bc549a99cab4b100d8dfd5d1
abportillo02 <abportillo@coh.org>
2025-10-17 13:59:29 -0700

    mafft analysis

---
+++ scripts/MAFFT_alignment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=mafft_alignment
+#SBATCH --output=/home/abportillo/github_repo/Aging/mafft/mafft_alignment.out
+#SBATCH --error=/home/abportillo/github_repo/Aging/mafft/mafft_alignment.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=abportillo@coh.org
+#SBATCH -n 16
+#SBATCH -N 1
+#SBATCH -p all
+#SBATCH --mem=50G
+#SBATCH --time=12:00:00
+
+source /home/abportillo/.bashrc
+conda activate /home/abportillo/.conda/envs/mamba_abner_BC
+mafft="/home/abportillo/.conda/envs/mamba_abner_BC/bin/mafft"
+
+${mafft} --auto --thread 16 /home/abportillo/github_repo/Aging/mafft/ltr7up_hervh.fasta > /home/abportillo/github_repo/Aging/mafft/ltr7up_hervh_aligned.fasta
