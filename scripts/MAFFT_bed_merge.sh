cc02601d610878949d64d5f7b472989ed3f9fdbb
abportillo02 <abportillo@coh.org>
2025-10-17 15:24:42 -0700

    merge again

--- scripts/MAFFT_bed_merge.sh
+++ scripts/MAFFT_bed_merge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=merge_ltr7up_dmrs
-#SBATCH --output=/home/abportillo/github_repo/Aging/mafft/merge_ltr7up_dmrs.out
-#SBATCH --error=/home/abportillo/github_repo/Aging/mafft/merge_ltr7up_dmrs.err
+#SBATCH --output=/home/abportillo/github_repo/Aging/mafft/merge_ltr7up_dmrs_labeled.out
+#SBATCH --error=/home/abportillo/github_repo/Aging/mafft/merge_ltr7up_dmrs_labeled.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=abportillo@coh.org
 #SBATCH -n 16 # Number of cores
@@ -19,10 +19,10 @@ bedtools="/home/abportillo/.conda/envs/mamba_abner_BC/bin/bedtools"
 
 
 # Extract first 4 columns from hg19-elife-datafile.txt (LTR7up elements)
-awk 'NR>1 {print $1"\t"$2"\t"$3"\tLTR" $4}' hg19-elife-datafile.txt > "${outdir}/ltr7up_labeled.bed"
+awk 'NR>1 {print $1"\t"$2"\t"$3"\tLTR" $4}' /home/abportillo/github_repo/Aging/mafft/hg19-elife-datafile.txt > "${outdir}/ltr7up_labeled.bed"
 
 # Add "HERVH" label to all entries in HERVH-DMRs.bed
-awk '{print $1"\t"$2"\t"$3"\tHERVH"}' HERVH-DMRs.bed > "${outdir}/hervh_labeled.bed"
+awk '{print $1"\t"$2"\t"$3"\tHERVH"}' /home/abportillo/github_repo/Aging/mafft/HERVH-DMRs.bed > "${outdir}/hervh_labeled.bed"
 
 # Concatenate both into a unified BED file
 cat ltr7up_labeled.bed hervh_labeled.bed > "${outdir}/merged_ltrs_labeled.bed"
