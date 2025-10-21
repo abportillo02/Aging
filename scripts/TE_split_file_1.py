f083bdace3eda3f7416c53dbe1e7b250cf6e90d7
abportillo02 <abportillo@coh.org>
2025-10-21 09:31:51 -0700

    Te split

--- scripts/TE_split_file_1.py
+++ scripts/TE_split_file_1.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
-
-OUTDIR = "/home/abportillo/github_repo/Aging/overlap"
-
+import sys
 from Bio import AlignIO
 
-alignment = AlignIO.read("/home/abportillo/github_repo/Aging/mafft/ltr7up_hervh_aligned.fasta", "fasta")
+bed_file = "/home/abportillo/github_repo/Aging/mafft/merged_ltrs_labeled.bed"
+fasta_file = "/home/abportillo/github_repo/Aging/mafft/ltr7up_hervh_aligned.fasta"
+out_file = "/home/abportillo/github_repo/Aging/overlap/TE_split_full.txt"
+
+# Read alignment
+alignment = AlignIO.read(fasta_file, "fasta")
 size = alignment.get_alignment_length()
 
-with open(f"{OUTDIR}/TE_split.txt", "w") as out:
-    for record in alignment:
+# Read BED
+bed_data = []
+with open(bed_file) as bed:
+    for line in bed:
+        s = line.strip().split()
+        bed_data.append((s[0], int(s[1]), int(s[2]), s[3]))  # chr, start, end, TE_name
+
+# Generate split file
+with open(out_file, "w") as out:
+    for chr_, start, end, te_name in bed_data:
+        length = end - start
+        step = length / size
         for i in range(size):
-            out.write(f"{record.id}\t{i}\n")
+            pos_start = int(start + i * step)
+            pos_end = pos_start + 1
+            out.write(f"{chr_}\t{pos_start}\t{pos_end}\t{i}\t{te_name}\n")
