08550ee0c2b3d291f40bb31d3ee1723f988dce9c
abportillo02 <abportillo@coh.org>
2025-10-21 09:42:21 -0700

    signal overlap

--- scripts/exo_signal_overlap_2.sh
+++ scripts/exo_signal_overlap_2.sh
@@ -14,11 +14,11 @@ source /home/abportillo/.bashrc
 conda activate /home/abportillo/.conda/envs/mamba_abner_BC
 
 CHIP_EXO="/net/nfs-irwrsrchnas01/labs/dschones/bioresearch/qianhui/projects/PMM/chipExo/GSE78099/GSM2466684_ZNF90_peaks_processed_score_signal_exo.bed.gz"
-SPLIT_FILE="/home/abportillo/github_repo/Aging/overlap/TE_split.txt"
+SPLIT_FILE="/home/abportillo/github_repo/Aging/overlap/TE_split_full.txt"
 BEDTOOLS="/home/abportillo/.conda/envs/mamba_abner_BC/bin/bedtools"
 OUTDIR="/home/abportillo/github_repo/Aging/overlap"
 
 # Step 1: Intersect TE regions with ChIP-exo peaks, keep all TE rows even if no overlap
 
 ${BEDTOOLS} intersect -a ${SPLIT_FILE} -b ${CHIP_EXO} -loj | \
-awk 'BEGIN{OFS="\t"} {score=$NF; if(score=="-1") score=0; print $1,$2,score}' > ${OUTDIR}/temp-ZNF90.counts
+awk 'BEGIN{OFS="\t"} {score=$11; if(score=="-1") score=0; print $1,$2,$3,$4,$5,score}' > ${OUTDIR}/temp-ZNF90.counts
