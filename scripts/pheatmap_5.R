32e6d8aefd9adcf0ce2aa752a7f4bd5cb6024887
abportillo02 <abportillo@coh.org>
2025-10-21 11:19:21 -0700

    deleteing

--- scripts/pheatmap_5.R
+++
@@ -1,9 +0,0 @@
-library(pheatmap)
-mat <- read.table("ZNF90.ordered.matrix", header=FALSE, row.names=1)
-mat_norm <- t(scale(t(mat)))  # z-score per TE
-pheatmap(mat_norm,
-         cluster_rows=TRUE,
-         cluster_cols=FALSE,
-         color=colorRampPalette(c("blue","white","red"))(100),
-         show_rownames=FALSE,
-         main="Base-resolution ChIP-exo Signal Across TE Alignment")
