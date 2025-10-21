a15376eabd837a3110609e79176b232915c9a0da
abportillo02 <abportillo@coh.org>
2025-10-21 14:57:11 -0700

    mafft align

--- scripts/MAFFT_alignment.sh
+++ scripts/MAFFT_alignment.sh
@@ -12,6 +12,11 @@
 
 source /home/abportillo/.bashrc
 conda activate /home/abportillo/.conda/envs/mamba_abner_BC
+
+export TMPDIR=/tmp  # or use /scratch if needed
+
 mafft="/home/abportillo/.conda/envs/mamba_abner_BC/bin/mafft"
 
-${mafft} --auto --thread 16 /home/abportillo/github_repo/Aging/mafft/ltr7up_hervh.fasta > /home/abportillo/github_repo/Aging/mafft/ltr7up_hervh_aligned.fasta
+# Run MAFFT with input and output files explicitly
+${mafft} --auto --thread 16 /home/abportillo/github_repo/Aging/mafft/ltr7up_hervh.fasta \
+> /home/abportillo/github_repo/Aging/mafft/ltr7up_hervh_aligned.fasta
