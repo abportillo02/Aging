5ee387180fe274a62c489261f95733d710ecece5
abportillo02 <abportillo@coh.org>
2025-10-21 09:15:50 -0700

    generate split

--- scripts/generate_split.sh
+++ scripts/generate_split.sh
@@ -2,9 +2,13 @@
 #SBATCH --job-name=generate_split
 #SBATCH --output=generate_split.out
 #SBATCH --error=generate_split.err
-#SBATCH -n 1
-#SBATCH --mem=10G
-#SBATCH --time=01:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=abportillo@coh.org
+#SBATCH -n 16 # Number of cores
+#SBATCH -N 1-4 # Min - Max Nodes
+#SBATCH -p all
+#SBATCH --mem=150G
+#SBATCH --time=48:00:00
 
 module load python/3.9
 source /home/abportillo/.bashrc
