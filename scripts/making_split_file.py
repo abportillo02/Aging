6376390fde0965002742ba8434dde1d5316a089a
abportillo02 <abportillo@coh.org>
2025-10-22 10:08:23 -0700

    /

--- scripts/making_split_file.py
+++ scripts/making_split_file.py
@@ -1,7 +1,7 @@
-
 import re
 
-with open("/home/abportillo/github_repo/Aging/mafft/ltr7up_hervh_aligned.fasta") as fasta, open("home/abportillo/github_repo/Aging/mafft/split_file.tsv", "w") as out:
+with open("/home/abportillo/github_repo/Aging/mafft/ltr7up_hervh_aligned.fasta") as fasta, \
+     open("/home/abportillo/github_repo/Aging/mafft/split_file.tsv", "w") as out:
     for line in fasta:
         if line.startswith(">"):
             header = line.strip()[1:]
