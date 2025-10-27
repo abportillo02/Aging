1ffa732a77310e879f40ce97752c06d8fbf288d1
abportillo02 <abportillo@coh.org>
2025-10-27 12:26:14 -0700

    bam to bigwig

--- scripts/bam-to-bigwig.sh
+++ scripts/bam-to-bigwig.sh
@@ -1,21 +1,25 @@
-#!/bin/bash
+#!/bin/sh
+#SBATCH --job-name=chipexo_bigwig_conversion
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=abportillo@coh.org
+#SBATCH -n 16
+#SBATCH -N 1-4
+#SBATCH -p all
+#SBATCH --mem=150G
+#SBATCH --time=48:00:00
+#SBATCH --output=/home/abportillo/github_repo/Aging/chip-fastq/rnapreprocess/bigwig/chipexo_bigwig_conversion_%j.log
 
 source /home/abportillo/.bashrc
 conda activate /home/abportillo/.conda/envs/mamba_abner_BC
 
 cd /home/abportillo/github_repo/Aging/chip-fastq/rnapreprocess/bams
-chrom_sizes="/net/nfs-irwrsrchnas01/labs/dschones/bioresearch/qianhui/hg38_2024/hg38_p14/filtered_hg38_p14.chrom.sizes"  # Update if needed
+chrom_sizes="/net/nfs-irwrsrchnas01/labs/dschones/bioresearch/qianhui/hg38_2024/hg38_p14/filtered_hg38_p14.chrom.sizes"
 
 for bam in *_sorted.bam; do
   base=$(basename "$bam" _sorted.bam)
   echo "Processing $bam"
 
-  # Index BAM
   samtools index "$bam"
-
-  # Generate bedGraph
   bedtools genomecov -ibam "$bam" -bg > "${base}.bedGraph"
-
-  # Convert to bigWig
-  bedGraphToBigWig "${base}.bedGraph" "$chrom_sizes" "${base}.bw"
+  bedGraphToBigWig "${base}.bedGraph" "$chrom_sizes" "/home/abportillo/github_repo/Aging/chip-fastq/rnapreprocess/bigwig/${base}.bw"
 done
