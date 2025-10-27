294c29daf9b96b35bfb2f46b1da8b5c0d2909b6a
abportillo02 <abportillo@coh.org>
2025-10-27 14:32:31 -0700

    bam to bigwig

--- scripts/bam-to-bigwig.sh
+++ scripts/bam-to-bigwig.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=abportillo@coh.org
 #SBATCH -n 16
-#SBATCH -N 1-4
+#SBATCH -N 1
 #SBATCH -p all
 #SBATCH --mem=150G
 #SBATCH --time=48:00:00
@@ -13,13 +13,20 @@ source /home/abportillo/.bashrc
 conda activate /home/abportillo/.conda/envs/mamba_abner_BC
 
 cd /home/abportillo/github_repo/Aging/chip-fastq/rnapreprocess/bams
-chrom_sizes="/net/nfs-irwrsrchnas01/labs/dschones/bioresearch/qianhui/hg38_2024/hg38_p14/filtered_hg38_p14.chrom.sizes"
 
 for bam in *_sorted.bam; do
   base=$(basename "$bam" _sorted.bam)
   echo "Processing $bam"
 
   samtools index "$bam"
-  bedtools genomecov -ibam "$bam" -bg > "${base}.bedGraph"
-  bedGraphToBigWig "${base}.bedGraph" "$chrom_sizes" "/home/abportillo/github_repo/Aging/chip-fastq/rnapreprocess/bigwig/${base}.bw"
+
+  bamCoverage \
+    -b "$bam" \
+    -o "/home/abportillo/github_repo/Aging/chip-fastq/rnapreprocess/bigwig/${base}.bw" \
+    --binSize 1 \
+    --normalizeUsing CPM \
+    --extendReads \
+    --ignoreDuplicates \
+    --minMappingQuality 30
 done
+``
