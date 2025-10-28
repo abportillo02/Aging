3ed44398d54ab1914d096a1da6d64fab9cb71417
abportillo02 <abportillo@coh.org>
2025-10-28 16:49:15 -0700

    getfasta

--- scripts/MAFFT_get_fasta.sh
+++ scripts/MAFFT_get_fasta.sh
@@ -15,8 +15,8 @@ conda activate /home/abportillo/.conda/envs/mamba_abner_BC
 
 BEDTOOLS="/home/abportillo/.conda/envs/mamba_abner_BC/bin/bedtools"
 REFERENCE_FASTA="/home/abportillo/genomes/hg38/hg38_p14.fa"
-MERGED_BED="/home/abportillo/github_repo/Aging/mafft/merged_HERVH_LTR7up_hg38.bed" # only change this and pipeline should run smoothly
-OUTDIR="/home/abportillo/github_repo/Aging/mafft/ltr7up_hervh.fasta" ## will overwrite the past analysis but much easier for me 
+MERGED_BED="/home/abportillo/github_repo/Aging/mafft/merged_DMR_LTR_HERVH_hg38.bed" # only change this and pipeline should run smoothly
+OUTDIR="/home/abportillo/github_repo/Aging/mafft/dmr_ltr7up_hervh.fasta" 
 
 ${BEDTOOLS} getfasta -fi ${REFERENCE_FASTA} -bed ${MERGED_BED} -fo ${OUTDIR} -name
 
